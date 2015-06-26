64919fe63aa40ef92d682a4a0d2ab44ff579c71d
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-06-27 00:34:46 +0200

    Add events the Chord components

--- components/Bar.react.js
+++ components/Bar.react.js
@@ -7,13 +7,11 @@
 
   Bar = React.createClass({
     render: function() {
-      console.log(this.props.chords);
       var chordsComponents = this.props.chords.map(function(chord) {
         return (
-          <Chord key={Math.random()} raw={chord} />
+          <Chord key={Math.random()} initialRawChord={chord} />
         )
       });
-      console.log(chordsComponents);
       return (
         <div className="bar">
           <h1>Bar</h1>
--- components/chord.react.js
+++ components/chord.react.js
@@ -5,11 +5,46 @@
   var React = require('react');
 
   var Chord = React.createClass({
+
+    getInitialState: function() {
+      return {
+        raw: this.props.initialRawChord,
+        active: false
+      }
+    },
+
+    handleInput: function() {
+      var newRaw = this.refs.textInput.getDOMNode().value;
+      this.setState({raw: newRaw});
+    },
+
+    onDoubleClick: function(e) {
+      alert("Double Click! " + this.props.raw);
+    },
+
+    gainedFocus: function() {
+      console.log("Gained focus");
+      this.setState({active: true});
+    },
+
+    lostFocus: function() {
+      console.log("Lost focus");
+      this.setState({active: false});
+    },
+
     render: function() {
+      console.log("Rendering chord " + this.state.raw);
       return (
-        <p>{this.props.raw}</p>
+        <input type="text" className={"chord" + (this.state.active ? " active" : "")}
+                           onDoubleClick={this.onClick}
+                           onChange={this.handleInput}
+                           onFocus={this.gainedFocus}
+                           onBlur={this.lostFocus}
+                           ref="textInput"
+                           value={this.state.raw} />
       )
-    }
+    },
+
   });
 
   module.exports = Chord;
