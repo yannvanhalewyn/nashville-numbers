879be44b21900eca6f1d68435b44ece6bd801005
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-06-30 06:47:00 +0200

    Move dispatch code to dispatcher

--- app/sheet/dispatcher/sheetDispatcher.js
+++ app/sheet/dispatcher/sheetDispatcher.js
@@ -1,8 +1,33 @@
 (function() {
 
   var Dispatcher = require('flux').Dispatcher;
+  var SheetStore = require('../stores/sheetStore');
+  var SheetStoreDataManager = require('../stores/sheetStoreDataManager');
+  var SheetConstants = require('../sheetConstants');
 
   var SheetDispatcher = new Dispatcher();
+
+  SheetDispatcher.register(function(action) {
+    switch(action.actionType) {
+      case SheetConstants.UPDATE_CHORD_TEXT:
+        SheetStoreDataManager.updateChordText(action.id, action.text);
+        break;
+
+      case SheetConstants.APPEND_NEW_CHORD:
+        SheetStoreDataManager.appendNewChord(action.id, action.barID);
+        SheetStore.emitChange();
+        break;
+
+      case SheetConstants.APPEND_NEW_BAR:
+        SheetStoreDataManager.appendNewBar(action.id, action.rowID);
+        SheetStore.emitChange();
+        break;
+
+      default:
+        console.error("No such task - " + action.actionType);
+        break;
+    }
+  });
 
   module.exports = SheetDispatcher;
 
--- app/sheet/stores/sheetStore.js
+++ app/sheet/stores/sheetStore.js
@@ -1,11 +1,10 @@
 (function() {
 
-  var SheetDispatcher = require('../dispatcher/sheetDispatcher');
   var EventEmitter = require('events').EventEmitter;
   var SheetConstants = require('../sheetConstants');
+  var SheetStoreDataManager = require('./sheetStoreDataManager');
   var assign = require('object-assign');
   var deNormalize = require('./deNormalize');
-  var SheetStoreDataManager = require('./sheetStoreDataManager');
 
   var CHANGE_EVENT = 'change';
 
@@ -31,29 +30,6 @@
       this.removeListene(CHANGE_EVENT, callback);
     }
 
-  });
-
-  SheetDispatcher.register(function(action) {
-    switch(action.actionType) {
-      case SheetConstants.UPDATE_CHORD_TEXT:
-        SheetStoreDataManager.updateChordText(action.id, action.text);
-        // SheetStore.emitChange();
-        break;
-
-      case SheetConstants.APPEND_NEW_CHORD:
-        SheetStoreDataManager.appendNewChord(action.id, action.barID);
-        SheetStore.emitChange();
-        break;
-
-      case SheetConstants.APPEND_NEW_BAR:
-        SheetStoreDataManager.appendNewBar(action.id, action.rowID);
-        SheetStore.emitChange();
-        break;
-
-      default:
-        console.error("No such task - " + action.actionType);
-        break;
-    }
   });
 
   module.exports = SheetStore;
