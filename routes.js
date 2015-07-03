fa455d6ded90f7c9af1684080c36867e987592d1
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-07-04 01:09:23 +0200

    Add functionality to chose active page when loading dock template

--- routes.js
+++ routes.js
@@ -6,7 +6,10 @@
     index: function(req, res) {
       fs.readFile('./app/dummyNormalizedSmall.json', function(err, data) {
         if (err) throw err;
-        res.render('sheet', {state: JSON.stringify(JSON.parse(data))});
+        res.render('sheet', {
+          active: {active_dashboard: true},
+          state: JSON.stringify(JSON.parse(data))
+        });
       });
     }
   };
