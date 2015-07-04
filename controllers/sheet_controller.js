bef33910f49fb50ae5151614757ebe64b47f1fd9
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-07-04 07:55:47 +0200

    Implement adding default data to sheet upon creation

--- controllers/sheet_controller.js
+++ controllers/sheet_controller.js
@@ -21,18 +21,16 @@
     // SHOW
     app.get('/sheets/:id', function(req, res) {
       var id = req.params.id;
-      if (parseInt(id) === 15) {
-        res.redirect('/');
-      } else {
-        res.render('sheets', {active: {active_sheets: true}, num: req.params.id});
-      }
+      Sheet.findById(req.params.id, function(err, sheet) {
+        res.render('sheet', {active: {active_sheets: true}, state: sheet.data});
+      });
     });
 
     // CREATE
     app.post('/sheets', function(req, res) {
-      var sheet = new Sheet({title: req.body.title, artist: req.body.artist, data:""});
+      var sheet = new Sheet({title: req.body.title, artist: req.body.artist,
+                            data:_generateDefaultData(req.body.title, req.body.artist)});
       if (!req.body.title || !req.body.artist) {
-        console.log("Forbidden - no title or artist");
         res.sendStatus('403');
         return;
       }
@@ -40,11 +38,81 @@
         if (err) {
           console.log("ERROR!");
           console.log(err);
+          res.sendStatus('500')
         }
+        res.render('sheet', {
+          active: {active_sheets: true},
+          state: sheet.data
+        });
       });
-      res.send('created!');
     })
 
   };
+
+  function _generateDefaultData(title, artist) {
+    return JSON.stringify({
+      "entities": {
+        "sections": {
+          "section1": {
+            "id": "section1",
+            "name": "intro",
+            "rows": [
+              "row1"
+            ]
+          }
+        },
+        "rows": {
+          "row1": {
+            "id": "row1",
+            "bars": [
+              "bar1",
+              "bar2",
+              "bar3",
+              "bar4"
+            ]
+          }
+        },
+        "bars": {
+          "bar1": {
+            "id": "bar1",
+            "chords": ["chord1"]
+          },
+          "bar2": {
+            "id": "bar2",
+            "chords": ["chord2"]
+          },
+          "bar3": {
+            "id": "bar3",
+            "chords": ["chord3"]
+          },
+          "bar4": {
+            "id": "bar4",
+            "chords": ["chord4"]
+          }
+        },
+        "chords": {
+          "chord1": {
+            "id": "chord1"
+          },
+          "chord2": {
+            "id": "chord2"
+          },
+          "chord3": {
+            "id": "chord3"
+          },
+          "chord4": {
+            "id": "chord4"
+          }
+        }
+      },
+      "result": {
+        "title": title,
+        "artist": artist,
+        "sections": [
+          "section1"
+        ]
+      }
+    })
+  }
 
 }())
