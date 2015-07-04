5f9cdafb08977b1352ac1346ebd7a0cbd585d99b
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-07-04 20:48:38 +0200

    Initialize passport

--- server.js
+++ server.js
@@ -6,13 +6,29 @@ var exphbs     = require('express-handlebars');
 var fs         = require('fs');
 var mongoose   = require('mongoose');
 var config     = require('./config');
+var passport   = require('passport');
 
 // Setup port, view engine and body-parser
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || config.PORT);
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use('/', express.static(__dirname + '/public/'));
 app.use(bodyParser.urlencoded({extended: false}));
+
+// Passport serialisation
+passport.serializeUser(function(user, done) {
+  console.log("In SERIALIZE USER");
+  console.log(user);
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  console.log("In DESERIALIZE USER");
+  console.log(obj);
+  done(null, obj);
+});
+app.use(passport.initialize());
+// app.use(passport.session());
+
 
 // Home route
 app.get('/', routes.index);
@@ -33,7 +49,7 @@ mongoose.connection.on('error', function (err) {
   console.log('Could not connect to mongo server!');
   console.log(err);
 });
-mongoose.connect(config.db_url());
+mongoose.connect(config.db_url);
 
 // Start server
 app.listen(app.get('port'), function() {
