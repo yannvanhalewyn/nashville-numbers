60a5affa5e8214cf5d74eb1d4653ce5733d9a2c1
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-07-05 00:38:24 +0200

    Setup server to use cookieparser and session for persisting the user session, improved user (de)serialisation with a db request and storing the dbID, only start server if database connection

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-var express    = require('express');
-var bodyParser = require('body-parser');
-var app        = express();
-var routes     = require('./routes');
-var exphbs     = require('express-handlebars');
-var fs         = require('fs');
-var mongoose   = require('mongoose');
-var config     = require('./config');
-var passport   = require('passport');
+var express      = require('express');
+var bodyParser   = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
+var app          = express();
+var routes       = require('./routes');
+var exphbs       = require('express-handlebars');
+var fs           = require('fs');
+var mongoose     = require('mongoose');
+var config       = require('./config');
+var passport     = require('passport');
+var User = require('./models/user');
 
 // Setup port, view engine and body-parser
 app.set('port', process.env.PORT || config.PORT);
@@ -14,44 +17,53 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use('/', express.static(__dirname + '/public/'));
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser())
+app.use(session({ resave: false,
+                  secret: 'a dirty little secret',
+                  saveUninitialized: false
+}))
 
 // Passport serialisation
 passport.serializeUser(function(user, done) {
   console.log("In SERIALIZE USER");
   console.log(user);
-  done(null, user);
+  done(null, user._id);
 });
 passport.deserializeUser(function(obj, done) {
   console.log("In DESERIALIZE USER");
   console.log(obj);
-  done(null, obj);
+  User.findById(obj, done);
 });
 app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.session());
+app.use(printAuthenticated);
 
-
-// Home route
-app.get('/', routes.index);
-
-// Read in controllers
-fs.readdirSync('./controllers').forEach(function(file) {
-  if (file.match(/.+\.js$/)) {
-    var route = require('./controllers/' + file);
-    route.controller(app);
-  }
-});
+// Setup Routes
+routes(app);
 
 // Connect to DB
 mongoose.connection.on('open', function (ref) {
   console.log('Connected to mongo server.');
+  // Start server
+  app.listen(app.get('port'), function() {
+    console.log("Server listening on port " + app.get('port'));
+  });
 });
 mongoose.connection.on('error', function (err) {
-  console.log('Could not connect to mongo server!');
+  console.error('Could not connect to mongo server!');
   console.log(err);
+  process.exit(1);
 });
 mongoose.connect(config.db_url);
 
-// Start server
-app.listen(app.get('port'), function() {
-  console.log("Server listening on port " + app.get('port'));
-});
+
+
+// Route middleware to ensure authentication
+function printAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    console.log("MIDDLEWARE: Authenticated!");
+  } else {
+  console.log("MIDDLEWARE: Not authenticated..");
+  }
+  return next();
+}
