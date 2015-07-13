94ebab3f683a3aad1a0c74eb511518c4faba705b
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-07-13 22:04:44 +0200

    Implement and wrote specs for sheets#DELETE route

--- routers/sheets.js
+++ routers/sheets.js
@@ -11,6 +11,7 @@
   SheetsRouter.get('/:sheet_id/edit', ensureAuth, SheetsController.edit);
   SheetsRouter.post('/', ensureAuth, SheetsController.create);
   SheetsRouter.put('/:sheet_id', ensureAuth, SheetsController.update);
+  SheetsRouter.delete('/:sheet_id', SheetsController.destroy);
 
   module.exports = SheetsRouter;
 
