9d7696ad903a6874328902509c65e0cbff4e0fb9
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-07-16 21:54:07 +0200

    Liveusersearch: use arrow keys to toggle between suggestions
    (horrible native javascript fix, find a better solution)

--- app/friendslist/components/liveUserSearch.react.js
+++ app/friendslist/components/liveUserSearch.react.js
@@ -2,6 +2,13 @@
 
   "use strict";
 
+  var KEY_CODES = {
+    up: 38,
+    down: 40
+  }
+
+  var TABINDEX = -1;
+
   var React     = require('react')
     , SearchBar = require('./searchBar.react')
     , FriendActions = require('../actions/friendActions')
@@ -26,7 +33,7 @@
 
     render: function() {
       return (
-        <div className="live-user-search">
+        <div className="live-user-search" onKeyDown={this._onKeyDown}>
           <SearchBar onInput={this._onInput}/>
           <FriendSuggestions users={this.state.users} />
         </div>
@@ -41,6 +48,24 @@
         return this.setState({users: []});
       } else if (value.length > 1) {
         return FriendActions.searchForUsers(value);
+      }
+    },
+
+    // TODO: this is horrible but works, fix it dammit!
+    _onKeyDown: function(e) {
+      if (e.keyCode == KEY_CODES.down) {
+        var tabbables = document.getElementsByClassName("friend-suggestion"); //get all tabable elements
+        if (tabbables[TABINDEX + 1]) {
+          tabbables[++TABINDEX].focus();
+        }
+      } else if (e.keyCode == KEY_CODES.up) {
+        var tabbables = document.getElementsByClassName("friend-suggestion"); //get all tabable elements
+        if (tabbables[TABINDEX - 1]) {
+          tabbables[--TABINDEX].focus();
+        } else {
+          document.getElementsByClassName('user-search-field')[0].focus();
+          TABINDEX = -1;
+        }
       }
     }
   })
