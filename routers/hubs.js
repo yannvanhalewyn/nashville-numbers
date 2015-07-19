ac8425c3c0d31ab6f9090bd2cb6b3248dcbc6154
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-07-19 16:10:20 +0200

    Wrote getTargetHub middleware + specs

--- routers/hubs.js
+++ routers/hubs.js
@@ -2,14 +2,16 @@
 
   "use strict";
 
-  var HubsRouter     = require('express').Router()
+  var include = require('include')
+    , HubsRouter     = require('express').Router()
     , HubsController = require('../controllers/hubs_controller')
+    , getTargetHub = include('/middlewares/getTargetHub')
 
   HubsRouter.get('/', HubsController.index);
-  HubsRouter.get('/:hub_id', HubsController.show);
+  HubsRouter.get('/:hub_id', getTargetHub, HubsController.show);
   HubsRouter.post('/', HubsController.create);
-  HubsRouter.put('/:hub_id', HubsController.update);
-  HubsRouter.delete('/:hub_id', HubsController.destroy);
+  HubsRouter.put('/:hub_id', getTargetHub, HubsController.update);
+  HubsRouter.delete('/:hub_id', getTargetHub, HubsController.destroy);
 
   module.exports = HubsRouter;
 
